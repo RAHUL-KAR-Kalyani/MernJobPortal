631c517fb9dfc7d070ac875ae5da5db254531384
RK <108023178+RAHUL-KAR-Kalyani@users.noreply.github.com>
2025-02-28 18:17:53 +0530

    Update Browse.jsx

--- frontend/src/components/Browse.jsx
+++ frontend/src/components/Browse.jsx
@@ -27,7 +27,7 @@ const Browse = () => {
 	
 
 	return (
-		<div className='h-full'>
+		<div className='max-h-screen'>
 			<Navbar />
 			<div className='max-w-7xl mx-auto my-10'>
 				<h1 className='font-bold text-xl my-10'>Search Results ({allJobs.length})</h1>
