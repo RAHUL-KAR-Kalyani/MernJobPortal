f27979cba1fb584b7b8a2b943cdb031bd1c87951
Rahul Kar <26rahulkar1997@gmail.com>
2025-07-23 01:39:01 +0530

    api updated

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,16 +1,19 @@
 
 
 
-export const USER_API_ENDPOINT="http://localhost:8000/auth/user"
 
 
-export const JOB_API_ENDPOINT="http://localhost:8000/auth/job"
+
+export const USER_API_ENDPOINT="https://mernjobportal-backend.onrender.com/auth/user"
 
 
-export const APPLICATION_API_ENDPOINT="http://localhost:8000/auth/application"
+export const JOB_API_ENDPOINT="https://mernjobportal-backend.onrender.com/auth/job"
 
 
-export const COMPANY_API_ENDPOINT="http://localhost:8000/auth/company"
+export const APPLICATION_API_ENDPOINT="https://mernjobportal-backend.onrender.com/auth/application"
+
+
+export const COMPANY_API_ENDPOINT="https://mernjobportal-backend.onrender.com/auth/company"
 
 
 
