ff4260f0a274a646c9ddc714d472dc6facb56b17
Rahul Kaar <108023178+RAHUL-KAR-Kalyani@users.noreply.github.com>
2026-03-10 06:38:32 +0530

    Update API endpoints to new backend URL

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -4,16 +4,16 @@
 
 
 
-export const USER_API_ENDPOINT="https://mernjobportal-backend.onrender.com/auth/user"
+export const USER_API_ENDPOINT="https://mernjobportal-backend-vwnp.onrender.com/auth/user"
 
 
-export const JOB_API_ENDPOINT="https://mernjobportal-backend.onrender.com/auth/job"
+export const JOB_API_ENDPOINT="https://mernjobportal-backend-vwnp.onrender.com/auth/job"
 
 
-export const APPLICATION_API_ENDPOINT="https://mernjobportal-backend.onrender.com/auth/application"
+export const APPLICATION_API_ENDPOINT="https://mernjobportal-backend-vwnp.onrender.com/auth/application"
 
 
-export const COMPANY_API_ENDPOINT="https://mernjobportal-backend.onrender.com/auth/company"
+export const COMPANY_API_ENDPOINT="https://mernjobportal-backend-vwnp.onrender.com/auth/company"
 
 
 
