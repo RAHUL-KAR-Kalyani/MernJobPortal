97db60fd542f27e51ad85fbc1f1f9fe8b8572dfb
Rahul Kar <108023178+RAHUL-KAR-Kalyani@users.noreply.github.com>
2026-03-22 22:38:59 +0530

    Update API endpoints in constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,37 +1,8 @@
 
+export const USER_API_ENDPOINT="https://mernjobportal-backend-phf3.onrender.com/auth/user"
 
+export const JOB_API_ENDPOINT="https://mernjobportal-backend-phf3.onrender.com/auth/job"
 
+export const APPLICATION_API_ENDPOINT="https://mernjobportal-backend-phf3.onrender.com/auth/application"
 
-
-
-export const USER_API_ENDPOINT="https://mernjobportal-backend-vwnp.onrender.com/auth/user"
-
-
-export const JOB_API_ENDPOINT="https://mernjobportal-backend-vwnp.onrender.com/auth/job"
-
-
-export const APPLICATION_API_ENDPOINT="https://mernjobportal-backend-vwnp.onrender.com/auth/application"
-
-
-export const COMPANY_API_ENDPOINT="https://mernjobportal-backend-vwnp.onrender.com/auth/company"
-
-
-
-/*
-
-
-export const USER_API_ENDPOINT="https://mernjobportal-backend.onrender.com/auth/user"
-
-
-
-export const JOB_API_ENDPOINT="https://mernjobportal-backend.onrender.com/auth/job"
-
-
-export const APPLICATION_API_ENDPOINT="https://mernjobportal-backend.onrender.com/auth/application"
-
-
-export const COMPANY_API_ENDPOINT="https://mernjobportal-backend.onrender.com/auth/company"
-
-
-
-*/
+export const COMPANY_API_ENDPOINT="https://mernjobportal-backend-phf3.onrender.com/auth/company"
